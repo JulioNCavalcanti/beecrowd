3cbc5be35c0c7dc9e8adfe45c9abe1637c98c032
JulioNCavalcanti <jnc.nunescavalcanti@outlook.com>
2023-06-06 18:14:08 -0300

    adicionando Beecrowd1000 // Hello World!

---
+++ src/Beecrowd1000.java
@@ -0,0 +1,13 @@
+// Hello World!
+
+import java.util.Scanner;
+
+public class Beecrowd1000 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Hello World!");
+
+        sc.close();
+    }
+ }
